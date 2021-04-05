95034d2287023a02988e3767204ec578684814db
sacapuntas9 <sacapuntas9@gmail.com>
2021-04-05 19:02:05 -0400

    populate README

--- fast_api_challenge/index.py
+++ fast_api_challenge/index.py
@@ -107,5 +107,6 @@ def get_summary(db: Session = Depends(get_db), token: str = Depends(oauth2_schem
                                                api_version=VERSION)
     return response
 
+
 if __name__ == "__main__":
     uvicorn.run(app=app, host="127.0.0.1", port=5000, log_level="debug")
