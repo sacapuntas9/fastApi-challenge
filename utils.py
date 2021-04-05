5be3d267526d5bcf4898219dfcb613d42cbd8672
sacapuntas9 <sacapuntas9@gmail.com>
2021-04-05 16:12:25 -0400

    Update connectivity utilities to properly connect to Cloud SQL when a specific environment variable is set

--- utils.py
+++ utils.py
@@ -5,8 +5,15 @@ password = os.getenv("POSTGRES_PASSWORD", "")
 server = os.getenv("POSTGRES_SERVER", "db")
 db = os.getenv("POSTGRES_DB", "app")
 
+# Unix socket connection configuration for google cloud
+db_socket_dir = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
+cloud_sql_connection_name = os.environ["CLOUD_SQL_CONNECTION_NAME"]
+
 if user and password and server and db:
     SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{server}/{db}"
+    if cloud_sql_connection_name:
+        SQLALCHEMY_DATABASE_URL = f"{SQLALCHEMY_DATABASE_URL}?unix_socket={db_socket_dir}/{cloud_sql_connection_name}"
+
 else:
     SQLALCHEMY_DATABASE_URL = os.getenv("sqlalchemy_database_url")  #  eg. "sqlite:///netflix.db"
 
